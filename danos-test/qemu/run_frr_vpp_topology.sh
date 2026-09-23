43a489d60bdba783d0d43842f0aa6d5ffe80d611
DANOS-Open Project <i-danos@users.noreply.github.com>
2026-09-23 17:30:43 +0800

    test: expose FRR zserv proxy for host wire probe

--- danos-test/qemu/run_frr_vpp_topology.sh
+++ danos-test/qemu/run_frr_vpp_topology.sh
@@ -29,7 +29,7 @@ qemu-system-x86_64 -enable-kvm -cpu host -m 1024 -smp 1 \
   -boot order=c \
   -drive file="$T/frr-1.qcow2",if=virtio,format=qcow2,snapshot=on \
   -cdrom "$T/r1-seed.iso" \
-  -netdev user,id=internet \
+  -netdev user,id=internet,hostfwd=tcp:127.0.0.1:$ZAPI_PORT-:2600 \
   -device e1000,netdev=internet,addr=0x5,mac=52:54:00:11:01:00 \
   -netdev socket,id=mgmt,connect=127.0.0.1:$MGMT_PORT \
   -device e1000,netdev=mgmt,addr=0x6,mac=52:54:00:11:01:01 \
