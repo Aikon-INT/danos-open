cd3468260106d6b7e2905b6dbe452e146ddb4257
DANOS-Open Project <i-danos@users.noreply.github.com>
2026-09-23 19:19:20 +0800

    fix: align FRR BGP route type mapping

--- danos-fib/src/mapper/zapi_mapper.c
+++ danos-fib/src/mapper/zapi_mapper.c
@@ -35,9 +35,9 @@ static danos_route_proto_t map_frr_protocol(uint8_t type)
     case 1:  return DANOS_ROUTE_PROTO_KERNEL;
     case 2:  return DANOS_ROUTE_PROTO_CONNECTED;
     case 4:  return DANOS_ROUTE_PROTO_STATIC;
-    case 7:  return DANOS_ROUTE_PROTO_OSPF;
-    case 9:  return DANOS_ROUTE_PROTO_ISIS;
-    case 10: return DANOS_ROUTE_PROTO_BGP;
+    case 6:  return DANOS_ROUTE_PROTO_OSPF;
+    case 8:  return DANOS_ROUTE_PROTO_ISIS;
+    case 9:  return DANOS_ROUTE_PROTO_BGP;
     case 11: return DANOS_ROUTE_PROTO_OSPF;
     default: return DANOS_ROUTE_PROTO_UNSPEC;
     }
